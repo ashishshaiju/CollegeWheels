92746ea6a87c5c732c50028e3d7016fcfbc50e36
ashishshaiju <74810128+ashishshaiju@users.noreply.github.com>
2024-07-17 21:25:07 +0530

    check api structure

---
+++ app/api/v1/otp/route.jsx
@@ -0,0 +1,8 @@
+export async function GET(request) {
+  return new Response(JSON.stringify({ message: 'Hello' }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
